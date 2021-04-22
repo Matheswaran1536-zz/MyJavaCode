92e3326f6eefcbbd5886e65367f5f8932c19d1af
matheswaran1536 <matheswaran1998@gmail.com>
2021-04-22 08:31:21 +0530

    with typecast

--- src/Basics/NumericPromotion.java
+++ src/Basics/NumericPromotion.java
@@ -9,5 +9,17 @@ public class NumericPromotion {
         //System.out.println(" x + y "+ x + y ); // 510.55 becuse it add string to double
 
         System.out.println(" X + Y = "+ (x+y));
+        
+        // Type casting 
+        
+       // int z = x + y;  Typecase problem double at RHS & INT at LHS
+        int z = x + (int)y;
+        
+        System.out.print("Z = "+z);
+     
+        short a = 14;
+        short b = 6;
+        short c = (short) (a/b);
+        System.out.println("C "+c);
     }
 }
