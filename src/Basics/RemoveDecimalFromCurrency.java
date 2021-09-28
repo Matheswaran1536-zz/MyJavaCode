1d751720f1855b0a5967d581004680401589c01f
matheswaran1536 <matheswaran1998@gmail.com>
2021-09-28 16:04:42 +0530

    Create RemoveDecimalFromCurrency.java
    return by removed decimal

---
+++ src/Basics/RemoveDecimalFromCurrency.java
@@ -0,0 +1,43 @@
+package Basics;
+
+
+import java.text.NumberFormat; 
+import java.util.Currency;
+import java.util.Locale;
+
+
+public class RemoveDecimalFromCurrency {
+	
+	public String FormatUSCurrencyWithDecimal(String value)
+	{
+	 
+	String formattedCurrency = "";
+	String amount = value;
+	int position = amount.indexOf("/");
+	if (position > 0) {
+	     amount= amount.substring(0, position);
+	}
+	formattedCurrency = amount.replace("$ ","").replace("$","").replace(",","").replace("%","");
+	
+	
+	double amt = Double.parseDouble(formattedCurrency);
+	
+	Locale locale = new Locale("en","US");
+	Currency currency = Currency.getInstance(locale);
+	NumberFormat numberFormat = NumberFormat.getCurrencyInstance(locale);
+	String output = numberFormat.format(amt);
+	String mainChapterNumber = output.split("\\.", 2)[0];
+	System.out.print(mainChapterNumber);
+	return output;
+	
+	
+	}
+	public static void main(String[] args) {
+		
+		
+		RemoveDecimalFromCurrency obj = new RemoveDecimalFromCurrency();
+		obj.FormatUSCurrencyWithDecimal("14,000");
+
+	}
+
+}
