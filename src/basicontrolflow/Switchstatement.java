8bd81b10bedf06e04f9e98372684a5edf303e959
matheswaran1536 <matheswaran1998@gmail.com>
2022-02-25 22:08:09 +0530

    completed basic control flow

---
+++ src/basicontrolflow/Switchstatement.java
@@ -0,0 +1,40 @@
+package basicontrolflow;
+
+public class Switchstatement {
+
+	public static void main(String[] args) {
+		
+		
+		int a =100;
+		
+		switch (a) {
+		
+		case 10 :
+		{
+			System.out.println("CASE 10 Block");
+			break;
+		}
+		case 20 :
+		{
+			System.out.println("CASE 20 Block");
+			break;
+
+		}
+		case 30 :
+		{
+			System.out.println("CASE 30 Block");
+			break;
+
+				
+		}
+		default:
+		{
+			System.out.println("Default Block");
+			break;
+
+		}
+
+	}
+
+}
+}
