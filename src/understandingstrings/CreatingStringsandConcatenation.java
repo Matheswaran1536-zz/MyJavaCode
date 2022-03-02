82cd98e7e71c44fd5583bfb7ec44de0c6f3cadab
Matheswaran R <matheswaran1998@gmail.com>
2022-03-02 11:09:51 +0530

    wip

--- src/understandingstrings/CreatingStringsandConcatenation.java
+++ src/understandingstrings/CreatingStringsandConcatenation.java
@@ -1,5 +1,16 @@
 package understandingstrings;
 
 public class CreatingStringsandConcatenation {
+	
+	String text = "helo";
+	String alttext = new String("Test");
+	
+	
+	int a = 10;
+	int b = 20;
+	String c = "Test String";
+	System.out.print("Test");
+	
+	//System.out.Print(a + b + c + a + b);
 
 }
